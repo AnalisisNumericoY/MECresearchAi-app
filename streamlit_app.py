b60860312f1fbffb2fbc7a06b2851a3a4e977726
= <=>
2024-12-20 11:35:55 -0500

    comienza

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 large language model ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "inspirado por Medicina Computacional [https://www.medicinacomputacional.com/](https://www.medicinacomputacional.com/)."
 )
