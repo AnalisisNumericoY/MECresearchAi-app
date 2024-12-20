5712aebbf8c0fdd199f724c8780c5218f3449a36
= <=>
2024-12-20 12:11:34 -0500

    comienza3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
-st.beta_set_page_config(page_title='MEC ia')
+from PIL import Image
+img = Image.open('logioto77.jpg')
+#st.beta_set_page_config(page_title='MEC ia', page_icon=img) #page_icon=':smiley:'
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="medicina computacional llm",
+    page_icon=img,
+    layout="wide")
 
 
-st.title("🎈 large language model ")
+#st.title("🎈 large language model ")
 st.write(
     "inspirado por Medicina Computacional [https://www.medicinacomputacional.com/](https://www.medicinacomputacional.com/)."
 )
