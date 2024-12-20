f1caed2a1fb4324716786a366ad8c3a7074c8207
= <=>
2024-12-20 12:23:07 -0500

    comienza7

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,8 @@ img = st.image('logioto77.jpg')
 
 import streamlit as st
 
-st.set_page_config(
-    page_title="medicina computacional llm",
-    page_icon="random",
-    layout="wide")
 
-
-#st.title("🎈 large language model ")
+st.title("🎈 large language model ")
 st.write(
     "inspirado por Medicina Computacional [https://www.medicinacomputacional.com/](https://www.medicinacomputacional.com/)."
 )
